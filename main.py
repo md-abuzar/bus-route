f5d1488200bb86774428c847b4d5f443c1fd001f
Mohammad Abuzar Ansari <mohdabuzar909@gmail.com>
2024-07-10 15:27:38 +0530

    updates

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify, request, json
 import pandas as pd
 from flask_cors import CORS
+from google.transit import gtfs_realtime_pb2
+import requests
+from geopy.geocoders import Nominatim
+
 routes_df = pd.read_csv("routes.csv")
 trips_df = pd.read_csv("trips.csv")
 stop_times_df = pd.read_csv("stop_times.csv")
@@ -9,6 +13,7 @@ stops_df = pd.read_csv("stops.csv")
 
 app = Flask(__name__)
 CORS(app)
+geolocator = Nominatim(user_agent="bus_route_App")
 
 @app.route("/api/v1/routes")
 def busnum():
@@ -40,17 +45,61 @@ def busroutes():
 
             stop_list = []
             for stop_id in stop_ids:
-                stops = stops_df[stops_df['stop_id'] == stop_id]['stop_name'].tolist()
-                stop_list.extend(stops)
+                stop_list.extend(stop_id)
             
             return jsonify({'stops': stop_list})
         else:
             return {"error" : "method not allowed"}
     except Exception as e:
         return {"error" : str(e)}
+    
 
 
+'''@app.route("/api/v1/track/live", methods=['GET', 'POST'])
+def buslive():
+    try:
+        if request.method == 'POST':
+            # Assuming route_short_name is passed as JSON data in the request body
+            route_short_name = request.json.get('route_short_name')
+            filtered_routes = routes_df[routes_df['route_long_name'] == route_short_name]
+            route_id = filtered_routes['route_id']
 
+            filtered_stop_ids = stop_times_df[stop_times_df['trip_id'] == route_id.to_list()[0]]
+            stop_ids = filtered_stop_ids['stop_id']
+
+            stop_list = []
+            for stop_id in stop_ids:
+                stop_data = stops_df[stops_df['stop_id'] == stop_id][['stop_name', 'stop_lat', 'stop_lon']].to_dict(orient='records')
+                stop_list.extend(stop_data)
+            return jsonify({'stops': stop_list})
+        else:
+            return {"error" : "method not allowed"}
+    except Exception as e:
+        return {"error" : str(e)}
+    
+
+
+    route_short_name = request.json.get('route_short_name')
+    filtered_routes = routes_df[routes_df['route_long_name'] == route_short_name]
+    route_id = filtered_routes['route_id'].tolist()[0]
+    
+    feed = gtfs_realtime_pb2.FeedMessage()
+    response = requests.get('https://otd.delhi.gov.in/api/realtime/VehiclePositions.pb?key=NBffniFfJNj2vHB67r3t0fdkVtkc76a0')
+    feed.ParseFromString(response.content)
+    lat_lon = []
+    for entity in feed.entity:
+        if entity.HasField('vehicle') and entity.vehicle.trip.route_id == str(route_id):
+            
+            location = geolocator.reverse((entity.vehicle.position.latitude, entity.vehicle.position.longitude), exactly_one=True)
+            print (location.address if location else "Unknown location")
+            return jsonify({
+                'latitude':entity.vehicle.position.latitude,
+                'longitude':entity.vehicle.position.longitude,        
+                })
+        
+            lat_lon.append(entity.vehicle.position.latitude, entity.vehicle.position.longitude)
+
+            print(lat_lon)'''
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
