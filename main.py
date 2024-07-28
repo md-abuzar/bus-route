f4e4d5aed410289779ecf5890c7924acd8850972
Mohammad Abuzar Ansari <mohdabuzar909@gmail.com>
2024-07-28 12:17:31 +0530

    main.py updated

--- main.py
+++ main.py
@@ -20,17 +20,6 @@ def busnum():
     routes = routes_df['bus_num'].tolist()
     return jsonify(routes)
 
-@app.route("/api/v1/num", methods=['GET', 'POST'])
-def busshortnum():
-    try:
-        route = request.json.get('route')
-        filtered_num = routes_df[routes_df['bus_num'] == route]
-        num = filtered_num['route_long_name'].tolist()
-        return jsonify(num)
-    except Exception as e:
-        return {"error" : str(e)}
-    
-
 @app.route("/api/v1/stops", methods=['GET', 'POST'])
 def busroutes():
     try:
@@ -56,42 +45,32 @@ def busroutes():
     
 
 
-'''@app.route("/api/v1/track/live", methods=['GET', 'POST'])
+@app.route("/api/v1/track/live", methods=['GET', 'POST'])
 def buslive():
     try:
         if request.method == 'POST':
-            # Assuming route_short_name is passed as JSON data in the request body
             route_short_name = request.json.get('route_short_name')
             filtered_routes = routes_df[routes_df['route_long_name'] == route_short_name]
-            route_id = filtered_routes['route_id']
-
-            filtered_stop_ids = stop_times_df[stop_times_df['trip_id'] == route_id.to_list()[0]]
-            stop_ids = filtered_stop_ids['stop_id']
-
-            stop_list = []
-            for stop_id in stop_ids:
-                stop_data = stops_df[stops_df['stop_id'] == stop_id][['stop_name', 'stop_lat', 'stop_lon']].to_dict(orient='records')
-                stop_list.extend(stop_data)
-            return jsonify({'stops': stop_list})
+            route_id = filtered_routes['route_id'].tolist()[0]
+            feed = gtfs_realtime_pb2.FeedMessage()
+            response = requests.get('https://otd.delhi.gov.in/api/realtime/VehiclePositions.pb?key=NBffniFfJNj2vHB67r3t0fdkVtkc76a0')
+            feed.ParseFromString(response.content)
+            lat_lon = []
+            for entity in feed.entity:
+                if entity.vehicle.trip.route_id == str(route_id):
+                    lat_lon.append((entity.vehicle.position.latitude, entity.vehicle.position.longitude))
+                
+            return jsonify({'live_lat_lon': lat_lon})
         else:
             return {"error" : "method not allowed"}
     except Exception as e:
         return {"error" : str(e)}
     
-
-
-    route_short_name = request.json.get('route_short_name')
-    filtered_routes = routes_df[routes_df['route_long_name'] == route_short_name]
-    route_id = filtered_routes['route_id'].tolist()[0]
     
-    feed = gtfs_realtime_pb2.FeedMessage()
-    response = requests.get('https://otd.delhi.gov.in/api/realtime/VehiclePositions.pb?key=NBffniFfJNj2vHB67r3t0fdkVtkc76a0')
-    feed.ParseFromString(response.content)
-    lat_lon = []
-    for entity in feed.entity:
-        if entity.HasField('vehicle') and entity.vehicle.trip.route_id == str(route_id):
+
+    
             
-            location = geolocator.reverse((entity.vehicle.position.latitude, entity.vehicle.position.longitude), exactly_one=True)
+    ''' location = geolocator.reverse((entity.vehicle.position.latitude, entity.vehicle.position.longitude), exactly_one=True)
             print (location.address if location else "Unknown location")
             return jsonify({
                 'latitude':entity.vehicle.position.latitude,
