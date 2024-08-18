194cb598c8046ba59c52100bc78c4b9dd12dd598
Mohammad Abuzar Ansari <mohdabuzar909@gmail.com>
2024-08-18 13:40:09 +0530

    updated pmpml

--- main.py
+++ main.py
@@ -10,10 +10,16 @@ trips_df = pd.read_csv("trips.csv")
 stop_times_df = pd.read_csv("stop_times.csv")
 stops_df = pd.read_csv("stops.csv")
 
+routes_pmpl_df = pd.read_csv("pmpl/routes.csv")
+trips_pmpl_df = pd.read_csv("pmpl/trips.csv")
+stop_times_pmpl_df = pd.read_csv("pmpl/stop_times.csv")
+stops_pmpl_df = pd.read_csv("pmpl/stops.csv")
+
 
 app = Flask(__name__)
 CORS(app)
 geolocator = Nominatim(user_agent="bus_route_App")
+
 
 @app.route("/api/v1/routes")
 def busnum():
@@ -95,6 +101,46 @@ def buslive():
             lat_lon.append(entity.vehicle.position.latitude, entity.vehicle.position.longitude)
 
             print(lat_lon)'''
+
+@app.route("/api/v1/pmpl/routes")
+def pmpmlbusnum():
+    routes = trips_pmpl_df['route_short_name'].tolist()
+    return jsonify(routes)
+
+@app.route("/api/v1/pmpml/num", methods=['GET', 'POST'])
+def pmpmlbusshortnum():
+    try:
+        route = request.json.get('route')
+        filtered_num = trips_pmpl_df[trips_pmpl_df['route_short_name'] == route]
+        num = filtered_num['route_id'].tolist()[0]
+        return jsonify(num)
+    except Exception as e:
+        return {"error" : str(e)}
+
+
+@app.route("/api/v1/pmpl/stops", methods=['GET', 'POST'])
+def pmpmlbusroutes():
+    try:
+        if request.method == 'POST':
+            route_id = request.json.get('route_id')
+            filtered_trips_df = trips_pmpl_df[trips_pmpl_df['route_id'] == route_id]
+            trip_id = filtered_trips_df['trip_id'].tolist()[0]
+
+            filtered_stop_times_df = stop_times_pmpl_df[stop_times_pmpl_df['trip_id'] == trip_id]
+            print(filtered_stop_times_df)
+            stop_ids = filtered_stop_times_df['stop_id'].tolist()
+            
+            stop_list = []
+            for stop_id in stop_ids:
+                stop_data = stops_pmpl_df[stops_pmpl_df['stop_id'] == stop_id][['stop_name', 'stop_lat', 'stop_lon']].to_dict(orient='records')
+                stop_list.extend(stop_data)
+            
+            return jsonify({'stops': stop_list})
+
+        else:
+            return {"error" : "method not allowed"}
+    except Exception as e:
+        return {"error" : str(e)}
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
